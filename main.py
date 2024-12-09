f61883feed0815ed93ca7caa1a5496c67919a049
Guilherme O Chagas <guilhermecchagas1999@gmail.com>
2024-12-09 03:48:46 -0300

    Contact page added with the button to go and return to /home

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def homepage():
+def index():
     return render_template("index.html")
+
+@app.route("/contato")
+def pagina_sobre():
+    return render_template("contato.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
