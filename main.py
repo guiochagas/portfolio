e7573b749c10e955b7893cfe746417d2a54d85e8
Guilherme O Chagas <guilhermecchagas1999@gmail.com>
2024-12-09 04:16:42 -0300

    Added the Flask function 'url_for' to link the pages and the frontend framework Bootstrap to build html templates

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def homepage():
     return render_template("index.html")
 
-@app.route("/contato")
-def pagina_sobre():
+@app.route("/contatos")
+def contatos(): # No "URL_FOR (que está sendo utilizado lá no html) nós usamos o nome da função como parâmetro"
     return render_template("contato.html")
 
 
