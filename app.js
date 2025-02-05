423d9ffc1da1ba295392843f7856cf8f0d0bd015
Guilherme O Chagas <guilhermecchagas1999@gmail.com>
2025-02-05 01:30:55 -0300

    openButton() JavaScript function launched and font-family changed

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 function openButton() {
-   
     let paragraph = document.querySelector(".main__first_paragraph");
-    let newText = "Meu nome é GUILHERME";
+    let newText = " Meu nome é GUILHERME";
 
-    paragraph.appendChild(newText);
-    
-    console.log("Text added")
+    let button = document.querySelector(".button_on_text");
+    button.remove();
 
+    let newTextSpan = document.createElement("span");
+    newTextSpan.textContent = newText;
+
+    paragraph.appendChild(newTextSpan);
+
+    newTextSpan.style.color = "#42de80";
+    newTextSpan.style.transition = "background-color 0.7s ease, border-radius 0.7s ease";
+    newTextSpan.style.borderRadius = "10px";
+    newTextSpan.style.padding = "2px";
+    newTextSpan.style.backgroundColor = "#868181";
+
+    setTimeout(() => {
+        newTextSpan.style.backgroundColor = "transparent";
+    }, 10);
 }
